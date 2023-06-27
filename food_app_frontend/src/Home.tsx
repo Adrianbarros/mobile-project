5e3287188ac8503f349ae240963b9936c228c235
Adrian Barros <adriansbarros1@gmail.com>
2023-06-26 23:14:25 -0400

    Added yarn and nav pacakges

---
+++ food_app_frontend/src/Home.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+const Home = () => {
+    return (<View>
+        <Text>Home</Text>
+    </View>);
+}
+
+export default Home;
