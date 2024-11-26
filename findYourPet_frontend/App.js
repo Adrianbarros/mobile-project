a24825451e30e73389c9cb6a4768d368236e15ee
Adrian Barros <adriansbarros1@gmail.com>
2024-11-25 23:02:59 -0500

    Added navegation bar to app

--- findYourPet_frontend/App.js
+++ findYourPet_frontend/App.js
@@ -1,10 +1,12 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { HomeStack } from './src/navigation/homeStack';
+import NavegationBar from './src/components/navegationBar';
 
 export default function App() {
   return (
     <NavigationContainer  >
       <HomeStack />
+      <NavegationBar />
     </NavigationContainer>
 
   );
