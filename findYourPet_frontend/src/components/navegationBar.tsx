c2dbf023c16670b6e8c2a268cab1283e6d91dec0
Adrian Barros <adriansbarros1@gmail.com>
2024-12-02 23:29:39 -0500

    Added styleSheet to navegationBar

--- findYourPet_frontend/src/components/navegationBar.tsx
+++ findYourPet_frontend/src/components/navegationBar.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { StyleSheet } from 'react-native/Libraries/StyleSheet/StyleSheet'
+
 
 function NavegationBar() {
     return (
         <div>Categories</div>
     )
 }
+const styles = StyleSheet.create({
+    container: {
 
+    },
+    navButton: {
+    },
+
+})
 
 export default NavegationBar
